fd5889e73d48d666f38f44446bf7b17bc2e42cc4
Chris Farmer <farmerc@udel.edu>
2020-03-06 23:54:46 -0500

    3/4 method finished and passed tests

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,59 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    private T[] array;
+
+    public ArrayUtility(T[] array){
+        this.array = array;
+    }
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        Integer count = 0;
+        for (T object : array) {
+            if (object.equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        for (T object : arrayToMerge) {
+            if (object.equals(valueToEvaluate)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        Integer count = 0;
+        for (T object : array) {
+            if(object.equals(valueToEvaluate))
+                count++;
+        }
+        return count;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        @SuppressWarnings("unchecked")
+        T[] buffer = (T[]) Array.newInstance(valueToRemove.getClass(), array.length);
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (!array[i].equals(valueToRemove)){
+                buffer[index++] = array[i];
+            }
+        }
+        @SuppressWarnings("unchecked")
+        T[] output = (T[]) Array.newInstance(valueToRemove.getClass(), index);
+        for (int i = 0; i < index; i++) {
+            output[i] = buffer[i];
+        }
+        return output;
+    }
 }
